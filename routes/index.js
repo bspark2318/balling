af745435a62e81882ce04df2581f2c177110c68b
bspark2318 <bspark2318@gmail.com>
2020-12-24 15:33:25 +0900

    Commit. Working on displaying results

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,28 @@ const url = require('url');
 // Require the scaper to use in router
 let statDriver = require('../public/javascripts/crawler');
 var router = express.Router();
+var numPlayers = 13;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'They See Me Balling', numPlayers: 13});
+  res.render('index', { title: 'They See Me Balling', numPlayers: numPlayers});
 });
 
 
 router.post('/', function(req, res, next) {
   // Set up
-  let players = (req.body.playerName);
+  let players = req.body.playerName;
+  let managers = req.body.managerName;
   let numberHalf = players.length / 2;
   let leftTeam = players.slice(0, numberHalf);
   let rightTeam = players.slice(numberHalf);
   let teams = [leftTeam, rightTeam];
 
+  // Passing Session data
   let sessionData;
   sessionData = req.session;
   sessionData.teams = {teams};
-
+  sessionData.managers = managers; 
   
   res.redirect('/loading');
 });
@@ -42,10 +45,44 @@ router.post('/loading', async function(req, res) {
 })
 
 router.get('/balling', function(req, res) {
-  let resultStats = req.session.stats;
-  console.log(resultStats);
-  res.render('balling');
+    // Set up
+    let statMap = { "trb_per_g": "Rebounds", "ast_per_g": "Assists", "blk_per_g": "Blocks", "stl_per_g": "Steals", "fg3_per_g": "Three Pointers", "pts_per_g": "Points", "fg_percentage": "Field Goal %", "ft_percentage": "Free Throw %"};
+    let resultStats = req.session.stats;
+    let managers = req.session.managers;
+    let leftManager = managers[0] === "" ? "Home Team" : managers[0] + "'s Team";
+    let rightManager = managers[1] === "" ? "Away Team" : managers[1] + "'s Team";
+    let statCategories = ["Points", "Assists", "Three Pointers", "Steals", "Free Throw %", "Rebounds", "Blocks", "Field Goal %" ];  
+    let leftTeam = reMapStats(resultStats[0], statCategories, statMap);
+    let rightTeam = reMapStats(resultStats[1], statCategories, statMap);
+    let numCategories = statCategories.length;
+    
+    let context = {numCategories: numCategories, leftManager: leftManager, rightManager: rightManager, leftTeam: leftTeam, rightTeam:rightTeam};
+    res.render('balling', context);
 })
+
+
+function reMapStats(stats, statCategories, statMap) {
+  let newStatMap = {};
+  let newCat, newValue;
+  
+  for (const [key, value] of Object.entries(stats)) {
+    newCat = statMap[key];
+    newStatMap[newCat] = value;
+  }
+
+  let newStatList = [];
+  
+  statCategories.forEach( (item, index) => {
+    newValue = newStatMap[item];
+    newStatList.push(newValue);
+  }) 
+
+  return newStatList;
+}
+
+
+
+
 
 module.exports = router;
 
